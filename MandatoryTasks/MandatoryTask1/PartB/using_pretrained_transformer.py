02400e1fe1618081b98b3dd5e401b9ed34d98b45
santoshcodes <santoshinnovates@gmail.com>
2025-10-17 16:47:05 +0530

    Update using_pretrained_transformer.py

--- MandatoryTasks/MandatoryTask1/PartB/using_pretrained_transformer.py
+++ MandatoryTasks/MandatoryTask1/PartB/using_pretrained_transformer.py
@@ -1,3 +1,5 @@
+"""First, i took a pre-trained model called Helsinki-NLP/opus-mt-en-fr.
+i loaded its tokenizer, which is like a tiny assistant that breaks sentences into words and numbers the model can understand"""
 from datasets import load_dataset
 from transformers import MarianMTModel, MarianTokenizer
 import torch, random
@@ -11,10 +13,13 @@ model = MarianMTModel.from_pretrained(model_name)
 
 dataset = load_dataset("ag_news", split="train")
 
-
+#Then, I randomly picked 5 sentences from this dataset
 def get_random_sentences(n=5):
     return [random.choice(dataset)["text"] for _ in range(n)]
-
+#Before the translator can work, the sentences need to be converted into tokens,which the model can understand!
+#the model generates translations from English tokens to French tokens.
+#The model does not  output readable text at first it just gives numbers and special tokens
+# therefore We use the tokenizer again to decode the tokens back into normal French sentences
 def translate(text_list):
     inputs = tokenizer(text_list, return_tensors="pt", padding=True, truncation=True)
     translated_tokens = model.generate(**inputs)
